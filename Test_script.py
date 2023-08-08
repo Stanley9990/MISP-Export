4d2180698dcd8850d00df99a70a3e30403a5cfce
Stanley <51371819+Stanley9990@users.noreply.github.com>
2023-08-08 08:56:35 +0000

    Created Test_script.py : For Pre-Deployment tests

---
+++ Test_script.py
@@ -0,0 +1,48 @@
+from pymisp import PyMISP, MISPEvent
+
+# Replace these with your MISP instance credentials
+misp_url = 'https://misp-instance-url'
+misp_key = 'your-api-key'
+misp_verifycert = False  # Set to True if using SSL/TLS certificate verification
+
+misp = PyMISP(misp_url, misp_key, misp_verifycert)
+
+# Fetch all events from MISP
+events = misp.search(eventinfo='', pythonify=True)
+
+# Lists to store extracted data
+domains = []
+ips = []
+urls = []
+
+for event in events:
+    misp_event = MISPEvent()
+    misp_event.load(event)
+
+    # Extract domains
+    for obj in misp_event.objects:
+        if obj.name == 'domain':
+            print(obj.value)
+            domains.append(obj.value)
+            
+        # Extract IP addresses
+        elif obj.name == 'ip-src' or obj.name == 'ip-dst':
+            print(obj.value)
+            ips.append(obj.value)
+
+        # Extract URLs
+        elif obj.name == 'url':
+            print(obj.value)
+            urls.append(obj.value)
+
+# Export domains to a file
+with open('domains.txt', 'w') as f:
+    f.write('\n'.join(domains))
+
+# Export IP addresses to a file
+with open('ips.txt', 'w') as f:
+    f.write('\n'.join(ips))
+
+# Export URLs to a file
+with open('urls.txt', 'w') as f:
+    f.write('\n'.join(urls))
