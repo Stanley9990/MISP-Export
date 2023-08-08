3ffe92ea7b5ccc4ba028a93999fce87fc7ab8119
Stanley <51371819+Stanley9990@users.noreply.github.com>
2023-08-08 23:22:25 +0000

    Cleaning Up...

--- Export.py
+++ Export.py
@@ -1,45 +1,72 @@
-from pymisp import PyMISP, MISPEvent
+import configparser
+from pymisp import PyMISP
 
-# Replace these with your MISP instance credentials
-misp_url = 'https://misp-instance-url'
-misp_key = 'your-api-key'
-misp_verifycert = False  # Set to True if using SSL/TLS certificate verification
+def main():
+    # Read configuration from config.ini file
+    config = configparser.ConfigParser()
+    config.read('config.ini')
 
-misp = PyMISP(misp_url, misp_key, misp_verifycert)
+    misp_url = config.get('misp', 'url')
+    misp_key = config.get('misp', 'key')
+    misp_verifycert = config.getboolean('misp', 'verifycert')
 
-# Fetch all events from MISP
-events = misp.search(eventinfo='', pythonify=True)
+    # Lists to store extracted attributes
+    domains = []
+    ips = []
+    urls = []
 
-# Lists to store extracted data
-domains = []
-ips = []
-urls = []
+    #Define Common Names
+    ip_common_names = ['ip-src', 'ip-dst', 'ip', 'ipv4-addr', 'ipv6-addr']
+    domain_common_names = ['domain', 'hostname', 'fqdn']
+    url_common_names = ['url']
 
-for event in events:
-    misp_event = MISPEvent()
-    misp_event.load(event)
+    #MISP connection
+    misp = PyMISP(misp_url, misp_key, misp_verifycert)
 
-    # Extract domains
-    for obj in misp_event.objects:
-        if obj.name == 'domain':
-            domains.append(obj.value)
 
-        # Extract IP addresses
-        elif obj.name == 'ip-src' or obj.name == 'ip-dst':
-            ips.append(obj.value)
+    #The following 3 For loops iterate through attribute names defined in the lists defined above
+    #They will grab all Attributes from the last 1d (One Day) and add them to the exported lists
+    #This could probably be turned into a function for elegance, but works for now
 
-        # Extract URLs
-        elif obj.name == 'url':
-            urls.append(obj.value)
+    for common_name in ip_common_names:
+        print(f"Current Attribute is {common_name}")
+        attributes = misp.search(controller='attributes', publish_timestamp='1d', type_attribute=common_name, pythonify=True)
+        for attribute in attributes:
+            print(attribute.value)
+            ips.append(attribute.value)
 
-# Export domains to a file
-with open('domains.txt', 'w') as f:
-    f.write('\n'.join(domains))
+    for common_name in domain_common_names:
+        print(f"Current Attribute is {common_name}")
+        attributes = misp.search(controller='attributes', publish_timestamp='1d', type_attribute=common_name, pythonify=True)
+        for attribute in attributes:
+            print(attribute.value)
+            domains.append(attribute.value)
 
-# Export IP addresses to a file
-with open('ips.txt', 'w') as f:
-    f.write('\n'.join(ips))
+    for common_name in url_common_names:
+        print(f"Current Attribute is {common_name}")
+        attributes = misp.search(controller='attributes', publish_timestamp='1d', type_attribute=common_name, pythonify=True)
+        for attribute in attributes:
+            print(attribute.value)
+            urls.append(attribute.value)
 
-# Export URLs to a file
-with open('urls.txt', 'w') as f:
-    f.write('\n'.join(urls))
+        # Export domains to a file
+    with open('export/domains.txt', 'w') as f:
+        f.write('\n'.join(domains))
+
+    # Export IP addresses to a file
+    with open('export/ips.txt', 'w') as f:
+        f.write('\n'.join(ips))
+
+    # Export URLs to a file
+    with open('export/urls.txt', 'w') as f:
+        f.write('\n'.join(urls))
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
