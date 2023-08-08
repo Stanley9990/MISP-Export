e27156a6414ea70d366965aa40cce830bb98cb13
Stanley <51371819+Stanley9990@users.noreply.github.com>
2023-08-08 23:42:19 +0100

    Update Alternate_test.py

--- Alternate_test.py
+++ Alternate_test.py
@@ -1,28 +1,19 @@
-import requests
-from configparser import ConfigParser
+import configparser
+from pymisp import PyMISP
 
-# Load configuration from config.ini
-config = ConfigParser()
+# Read configuration from config.ini file
+config = configparser.ConfigParser()
 config.read('config.ini')
 
 misp_url = config.get('misp', 'url')
-api_key = config.get('misp', 'key')
-verify_cert = config.getboolean('misp', 'verifycert')
+misp_key = config.get('misp', 'key')
+misp_verifycert = config.getboolean('misp', 'verifycert')
 
-# Construct the full URL for the download
-download_url = f"{misp_url}/attributes/text/download/ip-src"
+misp = PyMISP(misp_url, misp_key, misp_verifycert)
 
-# Set up the headers with the API key
-headers = {
-    "Authorization": f"Bearer {api_key}"
-}
 
-# Make the request
-response = requests.get(download_url, headers=headers, verify=verify_cert)
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Print the downloaded content
-    print(response.text)
-else:
-    print(f"Request failed with status code: {response.status_code}")
+attributes = misp.search(controller='attributes', publish_timestamp='1d', type_attribute='ip-src', pythonify=True)
+
+for attribute in attributes:
+    print(attribute.event_id, attribute)
