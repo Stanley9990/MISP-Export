ed2635a2515a5472f7e01a6b555f76542fcc11f5
Stanley <51371819+Stanley9990@users.noreply.github.com>
2023-08-15 06:33:57 +0000

    Updated Test_STIX

--- Tests/Test_STIX.py
+++ Tests/Test_STIX.py
@@ -18,9 +18,24 @@ def main():
     #MISP connection
     misp = PyMISP(misp_url, misp_key, misp_verifycert)
 
+    #The following 3 For loops iterate through attribute names defined in the lists defined above
+    #They will grab all Attributes from the last 1d (One Day) and add them to the exported lists
+    #This could probably be turned into a function for elegance, but works for now
 
-    events_stix = misp.search(controller='attributes', publish_timestamp='1d', pythonify=True, return_format="stix")
-    print(events_stix)
+    for common_name in ip_common_names:
+        print(f"Current Attribute is {common_name}")
+        attributes_ip = misp.search(controller='attributes', publish_timestamp='1d', type_attribute=common_name, pythonify=True, return_format=stix)
+        print(attributes_ip)
+
+    for common_name in domain_common_names:
+        print(f"Current Attribute is {common_name}")
+        attributes_domains = misp.search(controller='attributes', publish_timestamp='1d', type_attribute=common_name, pythonify=True, return_format=stix)
+        print(attributes_domains)
+
+    for common_name in url_common_names:
+        print(f"Current Attribute is {common_name}")
+        attributes_urls = misp.search(controller='attributes', publish_timestamp='1d', type_attribute=common_name, pythonify=True, return_format=stix)
+        print(attributes_urls)
 
 if __name__ == "__main__":
     main()
